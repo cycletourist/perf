80c173e0a55fb5b3551cd509392e7ea41f4d09c8
Mike Vermeulen <mike@vermeulen.com>
2023-12-16 14:33:48 -0600

    Update stream so we run it with two copies.

--- stream/run_stream.sh
+++ stream/run_stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /opt/AMD/aocc-compiler-4.1.0/bin/clang -O2 -fopenmp -mcmodel=large -ffp-contract=fast -fnt-store stream.c -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=100000000 -DNTIMES=100 -o stream
-./stream
+taskset -c 0,4 ./stream
