69eef7894cd76bf216a9e67ed4f5283f28f4b172
Mike Vermeulen <mike@vermeulen.com>
2023-12-18 20:18:55 -0600

    Run on performance cores.

--- stream/run_stream_intel.sh
+++ stream/run_stream_intel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/opt/intel/oneapi/compiler/2024.0/lib:$LD_LIBRARY_PATH
-/opt/intel/oneapi/compiler/2024.0/bin/icx -axCORE-AVX2 -O3 -qopenmp -mcmodel=large -qopt-streaming-stores always stream.c -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=100000000 -DNTIMES=100 -o stream
-taskset -c 0,4 ./stream
+/opt/intel/oneapi/compiler/2024.0/bin/icx -xCORE-AVX2 -O3 -qopenmp -mcmodel=large -qopt-streaming-stores always stream.c -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=100000000 -DNTIMES=100 -o stream
+taskset -c 0,2 ./stream
